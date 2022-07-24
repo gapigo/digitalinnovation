b31db65d6615e2c7e03ab955e0bff966c7028f22
gapigo <x.gabriel12@gmail.com>
2022-07-24 15:46:39 -0300

    Adding method "pessoaInfo()" in class Pessoa

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -5,11 +5,14 @@ class Pessoa {
     var cpf: String = "123.123.123-11"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 
 fun main() {
     val jether = Pessoa()
 
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.pessoaInfo())
 }
